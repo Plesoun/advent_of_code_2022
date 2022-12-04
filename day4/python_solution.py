d1451c53ca67bca22b4632412f7ad09e2e235f00
Jakub Schier <jakub.schier@gmail.com>
2022-12-04 22:51:05 +0100

    make tests pass

--- day4/python_solution.py
+++ day4/python_solution.py
@@ -2,5 +2,13 @@ import logging
 
 L = logging.getLogger(__name__)
 
-def range_comparison():
-    pass
+def range_comparison(range_1: str, range_2: str) -> bool:
+    # Parse the numbers from strings
+    range_1 = [int(i) for i in range_1.split("-")]
+    range_2 = [int(i) for i in range_2.split("-")]
+    if range_1[0] <= range_2[0] and range_1[1] >= range_2[1]:
+        print("True")
+        return True
+    else:
+        print("False")
+        return False
