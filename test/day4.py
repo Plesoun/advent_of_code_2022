83c202c1b02717917d3c08a098eb0c6538dd018d
Jakub Schier <jakub.schier@gmail.com>
2022-12-05 11:18:03 +0100

    cover all test cases

--- test/day4.py
+++ test/day4.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day4.python_solution import range_comparison
+from day4.python_solution import complete_overlap, partial_overlap
 
 
 class CampCleanupTestCase(unittest.TestCase):
@@ -8,8 +8,16 @@ class CampCleanupTestCase(unittest.TestCase):
         with open("./test/test_input_day3", "r") as f:
             self.Backpacks = f.readlines()
 
-    def test_range_comparison(self):
-        self.assertTrue(range_comparison(range_1="1-5", range_2="3-4"))
-        self.assertTrue(range_comparison(range_1="63-64", range_2="13-64"))
-        self.assertFalse(range_comparison(range_1="1-5", range_2="3-6"))
-        self.assertFalse(range_comparison(range_1="1-5", range_2="6-8"))
+    def test_complete_overlap(self):
+        self.assertTrue(complete_overlap(range_1="1-5", range_2="3-4"))
+        self.assertTrue(complete_overlap(range_1="63-64", range_2="13-64"))
+        self.assertFalse(complete_overlap(range_1="1-5", range_2="3-6"))
+        self.assertFalse(complete_overlap(range_1="1-5", range_2="6-8"))
+
+    def test_partial_overlap(self):
+        self.assertTrue(partial_overlap(range_1="1-5", range_2="3-4"))
+        self.assertTrue(partial_overlap(range_1="63-64", range_2="13-64"))
+        self.assertTrue(partial_overlap(range_1="1-5", range_2="3-6"))
+        self.assertTrue(partial_overlap(range_1="3-6", range_2="1-5"))
+        self.assertFalse(partial_overlap(range_1="1-5", range_2="6-8"))
+        self.assertFalse(partial_overlap(range_1="6-8", range_2="1-5"))
