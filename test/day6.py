36f85bea41f2575dfdb489a4ec868b8d423933b4
Jakub Schier <jakub.schier@gmail.com>
2022-12-06 08:51:23 +0100

    extend test for p2

--- test/day6.py
+++ test/day6.py
@@ -8,4 +8,10 @@ class SignalRecieverTestCase(unittest.TestCase):
         pass
 
     def test_signal_start(self):
-        self.assertEqual(start_of_packet_filter("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5)
+        self.assertEqual(start_of_packet_filter("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5)
+        self.assertEqual(start_of_packet_filter("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10)
+
+        self.assertEqual(start_of_packet_filter("nppdvjthqldpwncqszvftbrmjlhg", 14), 23)
+        self.assertEqual(start_of_packet_filter("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29)
+
+
