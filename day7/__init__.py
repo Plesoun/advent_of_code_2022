5276eeb6ecd1de60d95e099a23e5c6082d0f2e47
Jakub Schier <jakub.schier@gmail.com>
2022-12-07 10:47:08 +0100

    base day7 structure prep

---
+++ day7/__init__.py
@@ -0,0 +1,5 @@
+from .python_solution import dir_sizer
+
+__all__ = [
+    "dir_sizer"
+]
---
+++ test/day7.py
@@ -0,0 +1,8 @@
+import unittest
+
+from day7.python_solution import dir_sizer
+
+
+class DirSizerTestCase(unittest.TestCase):
+    def test_dir_sizer(self):
+        pass
